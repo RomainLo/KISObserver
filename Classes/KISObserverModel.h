dcb590fae1467f0121f40ad1ea4521eac60bdf4c
RomainLo <romain.lofaso@gmail.com>
2014-01-04 23:34:57 +0100

    Development of the observer models.

--- Classes/KISObserverModel.h
+++ Classes/KISObserverModel.h
@@ -8,14 +8,26 @@
 
 #import <Foundation/Foundation.h>
 
+/** The context when the KISObserverModel call observeValueForKeyPath:... */
+extern NSString * const kKISObserverModelContext;
+
+/** The definition of the block that will be call when KISObserverModelBlock is triggered */
+typedef void(^KISObserverBlock)(__weak id observed, NSDictionary *change);
+
+
+/** Immutable model that notifies the observer via observeValueForKeyPath: when it is triggered. */
 @interface KISObserverModel : NSObject
 
+/** The object which observes. */
 @property (nonatomic, weak, readonly) id observer;
 
+/** The object which is observed. */
 @property (nonatomic, weak, readonly) id observed;
 
+/** The option of the observation. */
 @property (nonatomic, assign, readonly) NSKeyValueObservingOptions options;
 
+/** The list of observed key paths. */
 @property (nonatomic, copy, readonly) NSArray *keyPaths;
 
 - (instancetype)initWithObserver:(id)observer
@@ -23,13 +35,19 @@
 								 options:(NSKeyValueObservingOptions)options
 								keyPaths:(NSArray *)keypaths;
 
+/**
+ Notify the observer that changes has happen for the given key path.
+ Subclasses override this method for implementing its own notification style.
+ @param keyPath The key path of the notification.
+					 It can't be nil and it should be contained in the keyPaths property.
+ @param change The change dictionnary of the notification. It can't be nil.
+ */
 - (void)triggerWithKeyPath:(NSString *)keyPath change:(NSDictionary *)change;
 
 @end
 
 
-typedef void(^KISObserverBlock)(__weak id observed, NSDictionary *change);
-
+/** Model that notifies the observer via a block when it is triggered. */
 @interface KISObserverModelBlock : KISObserverModel
 
 @property (nonatomic, copy, readonly) KISObserverBlock block;
@@ -39,5 +57,19 @@ typedef void(^KISObserverBlock)(__weak id observed, NSDictionary *change);
 								 options:(NSKeyValueObservingOptions)options
 								keyPaths:(NSArray *)keypaths
 									block:(KISObserverBlock)block;
+
+@end
+
+
+/** Model that notifies the observer via a selector when it is triggered. */
+@interface KISObserverModelSelector : KISObserverModel
+
+@property (nonatomic, assign, readonly) SEL selector;
+
+- (instancetype)initWithObserver:(id)observer
+								observed:(id)observed
+								 options:(NSKeyValueObservingOptions)options
+								keyPaths:(NSArray *)keypaths
+								selector:(SEL)selector;
 
 @end
--- Classes/KISObserverModel.m
+++ Classes/KISObserverModel.m
@@ -8,6 +8,8 @@
 
 #import "KISObserverModel.h"
 
+NSString * const kKISObserverModelContext = @"kis.observer.context";
+
 @implementation KISObserverModel
 
 - (instancetype)initWithObserver:(id)observer
@@ -15,6 +17,13 @@
 								 options:(NSKeyValueObservingOptions)options
 								keyPaths:(NSArray *)keypaths
 {
+	if (observed)
+		[[NSException exceptionWithName:NSInvalidArgumentException reason:@"The observed can't be nil." userInfo:nil] raise];
+	if (!observer)
+		[[NSException exceptionWithName:NSInvalidArgumentException reason:@"The observer can't be nil." userInfo:nil] raise];
+	if (![keypaths count])
+		[[NSException exceptionWithName:NSInvalidArgumentException reason:@"The keypaths can't be nil or empty." userInfo:nil] raise];
+	
 	self = [super init];
 	if (self) {
 		_observer = observer;
@@ -22,16 +31,18 @@
 		_options = options;
 		_keyPaths = [keypaths copy];
 	}
+	
 	return self;
 }
 
 - (void)triggerWithKeyPath:(NSString *)keyPath change:(NSDictionary *)change
 {
-	NSParameterAssert(keyPath);
-	NSParameterAssert(change);
-	NSAssert([self.keyPaths containsObject:keyPath], @"The given keypath isn't reconized");
-	
-	// Nothing to do; need to be overridden.
+	if (!change)
+		[[NSException exceptionWithName:NSInvalidArgumentException reason:@"The change dictionary can't be nil." userInfo:nil] raise];
+	if (![self.keyPaths containsObject:keyPath])
+		[[NSException exceptionWithName:NSInvalidArgumentException reason:@"The keypath can't be nil or unknown." userInfo:nil] raise];
+		
+	[self.observer observeValueForKeyPath:keyPath ofObject:self.observed change:change context:(__bridge void *)(kKISObserverModelContext)];
 }
 
 @end
@@ -45,17 +56,84 @@
 								keyPaths:(NSArray *)keypaths
 									block:(KISObserverBlock)block
 {
+	if (!block)
+		[[NSException exceptionWithName:NSInvalidArgumentException reason:@"The block can't be nil." userInfo:nil] raise];
+	
 	self = [super initWithObserver:observer observed:observed options:options keyPaths:keypaths];
-	if (self) {
+	if (nil == self) {
 		_block = [block copy];
 	}
+	
 	return self;
 }
 
 - (void)triggerWithKeyPath:(NSString *)keyPath change:(NSDictionary *)change
 {
-	[super triggerWithKeyPath:keyPath change:change];
+	if (!self.block) // bad initializer
+		return [super triggerWithKeyPath:keyPath change:change];
+	
+	if (!change)
+		[[NSException exceptionWithName:NSInvalidArgumentException reason:@"The change dictionary can't be nil." userInfo:nil] raise];
+	if (![self.keyPaths containsObject:keyPath])
+		[[NSException exceptionWithName:NSInvalidArgumentException reason:@"The keypath can't be nil or unknown." userInfo:nil] raise];
+		
 	self.block(self.observed, change);
+}
+
+@end
+
+
+@implementation KISObserverModelSelector
+
+- (instancetype)initWithObserver:(id)observer
+								observed:(id)observed
+								 options:(NSKeyValueObservingOptions)options
+								keyPaths:(NSArray *)keypaths
+								selector:(SEL)selector
+{
+	self = [super initWithObserver:observer observed:observed options:options keyPaths:keypaths];
+	
+	if (self) {
+		NSMethodSignature *methodSignature = [self.observer methodSignatureForSelector:selector];
+		NSInteger numberOfArguments = [methodSignature numberOfArguments];
+		if (!methodSignature || (2 < numberOfArguments))
+			[[NSException exceptionWithName:NSInvalidArgumentException reason:@"The selector should have 2 parameters maximum and be part of the observer." userInfo:nil] raise];
+		
+		_selector = selector;
+	}
+	
+	return self;
+}
+
+- (void)triggerWithKeyPath:(NSString *)keyPath change:(NSDictionary *)change
+{
+	NSMethodSignature *methodSignature = [self.observer methodSignatureForSelector:self.selector];
+	NSInteger numberOfArguments = [methodSignature numberOfArguments];
+	
+	if (!methodSignature) // bad initializer
+		return [super triggerWithKeyPath:keyPath change:change];
+	
+	if (!change)
+		[[NSException exceptionWithName:NSInvalidArgumentException reason:@"The change dictionary can't be nil." userInfo:nil] raise];
+	if (![self.keyPaths containsObject:keyPath])
+		[[NSException exceptionWithName:NSInvalidArgumentException reason:@"The keypath can't be nil or unknown." userInfo:nil] raise];
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Warc-performSelector-leaks"
+	switch (numberOfArguments) {
+		case 2: // No argument
+			[self.observer performSelector:self.selector];
+			break;
+		case 3: // 1 argument: the observed object
+			[self.observer performSelector:self.selector withObject:self.observed];
+			break;
+		case 4: // 2 argument: the observed object & the change dics
+			[self.observer performSelector:self.selector withObject:change];
+			break;
+		default:
+			NSAssert(NO, @"2 < numberOfArguments < 4");
+	}
+#pragma clang diagnostic pop
 }
 
 @end
